d993525d62f6f8fc2b1c9edac2a488beb9568cd3
Brayan-Prieto <brayanprietolozoya@gmail.com>
2025-02-06 15:14:18 -0600

    added story book

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Button from "./Button";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: 'Example/Button',
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Demo: Story = {
+  args: {
+    label: 'Button',
+  },
+};
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Button.css';
 
-interface ButtonInterface {
+export interface ButtonInterface {
     label:string
 }
 
